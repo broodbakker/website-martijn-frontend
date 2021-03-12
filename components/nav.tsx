9853cbe2c2ad68129fd321865545906008b84290
martijn van de ree <mrtjnvanderee@gmail.com>
2021-03-12 10:23:42 +0100

    typescript added

---
+++ components/nav.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Nav = () => (
+  <nav>
+    nav
+  </nav >
+)
+
+export default Nav
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Head from '../components/head'
+
+
+const Home = () => (
+  <div>
+    <Head title="Home" />
+    home
+  </div>
+)
+
+export default Home
